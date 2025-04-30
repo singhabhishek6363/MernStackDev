61c837ae4cd6f5e8d09110cb3fff0c6ddf695a7d
singhabhishek6363 <thakurabhisheksingh6363@gmail.com>
2025-04-30 23:48:48 +0530

    Primitive and non- primitive

---
+++ JavaScript/part2/primitive.js
@@ -0,0 +1,18 @@
+// Number
+
+let balance = 120
+let anotherBalence = new Number(120)
+
+
+// console.log(balance);
+// console.log(anotherBalence.valueOf());
+
+// console.log(typeof(balance));
+// console.log(typeof anotherBalence);
+
+// boolean
+let isActive= true
+let isReallyActive = new Boolean(true)
+console.log(isActive);
+
+
