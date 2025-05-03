d120e5e33d8e2fae5212900a435962dd9a0b4832
singhabhishek6363 <thakurabhisheksingh6363@gmail.com>
2025-05-04 01:15:23 +0530

    loops

---
+++ JavaScript/par5/loops.js
@@ -0,0 +1,36 @@
+// write a while loop that calculate sum of all numbers from 1 to 5 and stores the result in a bvariable sum..
+
+let sum = 0
+let i = 1
+
+while (i<=5) {
+    sum =sum + i
+    // sum += 1
+    i++
+    
+}
+
+console.log(sum);
+
+
+// write a while loop that count down from 5 to 1 and stores the numbers in an array named countdown
+
+let countdown = []
+let j =5
+while (j>0) {
+    countdown.push(j);
+    j--;
+
+
+    
+}
+console.log(countdown);
+
+// do- while loop
+
+let teaCollection = []
+let tea
+do {
+    
+    
+} while (condition);
